ed1f5f8446ab287bfa0cd560a6269b8171ed3610
kishoreramamurthy <164518669+kishoreramamurthy@users.noreply.github.com>
2024-03-26 21:43:40 +0530

    Update one.php

--- one.php
+++ one.php
@@ -95,7 +95,7 @@
         }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = $_POST['username'];
+            $username = $_POST['userdata'];
             $email = $_POST['email'];
 
             $sql = "INSERT INTO users (username, email) VALUES ('$username', '$email')";
